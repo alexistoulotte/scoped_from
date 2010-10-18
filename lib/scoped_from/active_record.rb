2bdb8bf223327bb82f2f543062c5036911387966
Alexis Toulotte <al@alweb.org>
2010-10-18 17:25:10 +0200

    added ActiveRecord::Base#scope_arity method

--- lib/scoped_from/active_record.rb
+++ lib/scoped_from/active_record.rb
@@ -7,13 +7,28 @@ module ScopedFrom
     end
 
     module ClassMethods
-
+      
+      def scope(name, scope_options, &block)
+        super
+        scope_arities[name] = scope_options.is_a?(Proc) ? scope_options.arity : -1
+      end
+      
+      def scope_arity(name)
+        scope_arities[name]
+      end
+      
       def scoped_from(params, options = {})
         scope = scoped
         (params || {}).each do |name, value|
           scope = scope.send(name, value) if scopes.key?(name.to_sym)
         end
         scope
+      end
+      
+      private
+      
+      def scope_arities
+        read_inheritable_attribute(:scope_arities) || write_inheritable_attribute(:scope_arities, {}.with_indifferent_access)
       end
 
     end
--- spec/scoped_from/active_record_spec.rb
+++ spec/scoped_from/active_record_spec.rb
@@ -2,6 +2,24 @@ require 'spec_helper'
 
 describe ScopedFrom::ActiveRecord do
   
+  describe '#scope_arity' do
+    
+    it 'is correct value' do
+      User.scope_arity(:enabled).should be(-1)
+      User.scope_arity(:search).should be(1)
+    end
+    
+    it 'is nil if specified name is undefined' do
+      User.scope_arity(:foo).should be_nil
+    end
+    
+    it 'is with indifferent access' do
+      User.scope_arity('search').should be(1)
+      User.scope_arity(:search).should be(1)
+    end
+    
+  end
+  
   describe '#scoped_from' do
     
     it 'adds scope specified as parameter' do
