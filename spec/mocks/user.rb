76741bfc3192bbb5433d2b13f768a04d4bbb2823
Alexis Toulotte <al@alweb.org>
2013-07-17 18:40:53 +1100

    new lambda syntax

--- spec/mocks/user.rb
+++ spec/mocks/user.rb
@@ -1,14 +1,8 @@
 class User < ActiveRecord::Base
 
   scope :enabled, where(enabled: true)
-  scope :search, lambda { |pattern|
-    where('firstname LIKE ? OR lastname LIKE ?', "%#{pattern}%", "%#{pattern}%")
-  }
-  scope :created_between, lambda { |after, before|
-    where('created_at >= ? AND created_at <= ?', after, before)
-  }
-  scope :latest, lambda {
-    where('created_at >= ?', 1.week.ago)
-  }
+  scope :search, -> (pattern) { where('firstname LIKE ? OR lastname LIKE ?', "%#{pattern}%", "%#{pattern}%") }
+  scope :created_between, -> (after, before) { where('created_at >= ? AND created_at <= ?', after, before) }
+  scope :latest, -> { where('created_at >= ?', 1.week.ago) }
 
 end
